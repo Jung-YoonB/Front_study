9573001fdca7066d3d0d4043ce21d51b52d7e969
Jung-YoonB <amazetong@naver.com>
2026-07-27 11:36:20 +0900

    #260727 [7월 5주차 실습] JS 실습-문법_04

---
+++ JavaScript/core/04_조건문.js
@@ -0,0 +1,35 @@
+/*
+    * 조건문
+        - if / else if / else
+        - switch
+        - 삼항 연산자
+*/
+
+// if
+const score = 80;
+// const score = prompt(`점수 입력 : `);
+let grade;
+
+if (score >= 90) grade = `A`;
+else if (score >= 80) grade = `B`;
+else if (score >= 70) grade = `C`;
+else grade = `D`;
+
+console.log(grade);
+
+// switch : 비교 대상과 특정 값을 비교할 때 일치 연산(===) 으로 비교
+const day = 3;
+
+switch (day) {
+    case 0: console.log(`일요일`);
+        break;
+    case 1: console.log(`월요일`);
+        break;
+    case 2: console.log(`화요일`);
+        break;
+    // case 3: console.log(`수요일`);    
+    case `3`: console.log(`수요일`);    // day == 3 이 아닌, day === 0 비교
+        break;
+    default: console.log(`목요일 이후, 또는 잘못 된 값`);
+        break;
+}
